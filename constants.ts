bbb4631cf4e8ec70e2d392246639746977a40acd
Yannick <yannlacroix@fastmail.com>
2024-10-05 14:40:00 +0200

    misc: establish first types and constants

---
+++ constants.ts
@@ -0,0 +1,25 @@
+export enum CardinalPoint {
+    NORTH = "N",
+    EAST = "E",
+    SOUTH = "S",
+    WEST = "W"
+}
+
+export enum FileElement {
+    MAP = "C",
+    ADVENTURER = "A",
+    TREASURE = "T",
+    MOUNTAIN = "M",
+}
+
+export enum GameElement {
+    ADVENTURER = "A",
+    TREASURE = "T",
+    MOUNTAIN = "M",
+}
+
+export enum Instruction {
+    FORWARD = "A",
+    LEFT = "G",
+    RIGHT = "D"
+}
---
+++ types.ts
@@ -0,0 +1,23 @@
+import { CardinalPoint } from "./constants"
+
+interface MapElement {
+    xAxis: number
+    yAxis: number
+}
+
+export interface Adventurer extends MapElement {
+    name: string,
+    orientation: CardinalPoint,
+    treasureCount: number,
+}
+
+export interface Mountain extends MapElement { }
+
+export interface Treasure extends MapElement {
+    count: number,
+}
+
+export interface Map {
+    width: number,
+    height: number,
+}
